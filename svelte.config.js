fae95de4d263fe9b3d40e9bbde462be32f2d0485
Imran Molla Joy <imranmollajoy@gmail.com>
2025-11-20 21:49:04 +0600

    depl

--- svelte.config.js
+++ svelte.config.js
@@ -7,7 +7,17 @@ const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: [vitePreprocess(), mdsvex()],
-	kit: { adapter: adapter() },
+	kit: {
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			strict: true
+		}),
+		paths: {
+			base: ''
+		}
+	},
 	extensions: ['.svelte', '.svx']
 };
 
